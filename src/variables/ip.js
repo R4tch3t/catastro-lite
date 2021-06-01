2e10c326cee5c5e7a1bd1fdfb6af890b0e80eeb0
R4tch3t <bebetovic@hotmail.com>
2021-05-31 20:01:07 -0500

    u

--- src/variables/ip.js
+++ src/variables/ip.js
@@ -1,7 +1,7 @@
 export default (port) => {
     //const ip = 'localhost'
     //const ip = 'catastro.sys.chilapadealvarez.mx'
-    const ip = 'predial.chilapadealvarez.mx'
+    const ip = 'chilapadealvarez.mx'
     //const ip = '192.168.1.94'
     //const ip = '74.208.88.161'
     //const ip = "www.chilapadealvarez.info"
